bd2249cb3766e80ec540419e57ad211110c061a6
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-06-06 21:18:27 +0900

    Step 0. 최초 프로젝트 구성

---
+++ rainyday/rainyday/Source/Module/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  rainyday
+//
+//  Created by JSilver on 2020/06/06.
+//  Copyright © 2020 JSilver. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
