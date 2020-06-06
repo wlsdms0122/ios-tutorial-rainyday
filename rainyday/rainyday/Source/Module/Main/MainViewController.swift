3e9b5aa56ff852880df884628733ebc14c5b291b
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-06-07 00:48:06 +0900

    Step 2. 날씨 정보를 통해 layout 갱신

--- rainyday/rainyday/Source/Module/Main/MainViewController.swift
+++ rainyday/rainyday/Source/Module/Main/MainViewController.swift
@@ -35,6 +35,10 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         // 현재 위치 정보를 가져 옴
         locationService.getLocation { [weak self] in
@@ -62,7 +66,9 @@ class MainViewController: UIViewController {
                 
                 // 날씨 정보를 얻은 경우
                 print(weather)
-                // TODO: Layout 정보 갱신
+                guard let daily = weather.daily.first else { return }
+                self?.setHeader(timeZone: weather.timezone, in: Date())
+                self?.setWeahter(current: weather.current, min: daily.temperature.minimum, max: daily.temperature.maximum)
             }
         }
     }
@@ -74,5 +80,41 @@ class MainViewController: UIViewController {
     private func setupLayout() {
         locationView.layer.cornerRadius = 12
         forecastView.layer.cornerRadius = 16
+    }
+    
+    /// 헤더 정보 설정
+    private func setHeader(timeZone: String, in date: Date) {
+        // 현재 위치
+        locationLabel.text = timeZone
+        
+        let formatter = DateFormatter()
+        // 날짜
+        formatter.dateFormat = "yyyy. MM. dd (E)"
+        dateLabel.text = formatter.string(from: date)
+        
+        // 시간
+        formatter.dateFormat = "a H:mm"
+        timeLabel.text = formatter.string(from: date)
+    }
+    
+    /// 날씨 정보 설정
+    private func setWeahter(current: Current, min: Double, max: Double) {
+        guard let weather = current.weather.first else { return }
+        // 날씨
+        rainydayLabel.text = weather.main == .rain ? "비와요" : "맑아요"
+        
+        // 현재 기온
+        currentTemperatureLabel.text = String(format: "%.2f", convert(kelvin: current.temperature))
+        // 최저 기온
+        minTemperatureLabel.text = String(format: "%.2f", convert(kelvin: min))
+        // 최고 기온
+        maxTemperatureLabel.text = String(format: "%.2f", convert(kelvin: max))
+        // 습도
+        humidityLabel.text = String(current.humidity)
+    }
+    
+    /// 켈빈 온도를 섭씨온도로 변환
+    private func convert(kelvin: Double) -> Double {
+        kelvin - 273.15
     }
 }
