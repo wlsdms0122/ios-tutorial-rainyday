7027d0a1cc1fa1f37d98e88ab9bb2a290a11a3de
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-06-06 23:55:39 +0900

    Step 2. Main outlet 연결 및 초기 layout 설정

--- rainyday/rainyday/Source/Module/Main/MainViewController.swift
+++ rainyday/rainyday/Source/Module/Main/MainViewController.swift
@@ -11,16 +11,36 @@ import CoreLocation
 
 class MainViewController: UIViewController {
     // MARK: - Layout
+    // 헤더 영역
+    @IBOutlet private weak var locationView: UIView!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    
+    // 날씨 정보 영역
+    @IBOutlet private weak var rainydayLabel: UILabel!
+    @IBOutlet private weak var currentTemperatureLabel: UILabel!
+    @IBOutlet private weak var minTemperatureLabel: UILabel!
+    @IBOutlet private weak var maxTemperatureLabel: UILabel!
+    @IBOutlet private weak var humidityLabel: UILabel!
+    
+    // 일기 예보
+    @IBOutlet private weak var forecastView: UIView!
     
     // MARK: - Property
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLayout()
     }
     
     // MARK: - Public
     
     // MARK: - Private
+    /// 초기 레이아웃 설정
+    private func setupLayout() {
+        locationView.layer.cornerRadius = 12
+        forecastView.layer.cornerRadius = 16
+    }
 }
-
