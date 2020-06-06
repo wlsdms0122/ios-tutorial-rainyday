619485e6559c92db7d3aa556ec778d9d98306fe7
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-06-06 23:06:20 +0900

    Step 2. Resource constants 추가

---
+++ rainyday/rainyday/Source/Common/Resource.swift
@@ -0,0 +1,31 @@
+//
+//  Resource.swift
+//  rainyday
+//
+//  Created by JSilver on 2020/06/06.
+//  Copyright © 2020 JSilver. All rights reserved.
+//
+
+import UIKit
+
+typealias R = Resource
+
+enum Resource {
+    // MARK: - Color
+    static let primary01: UIColor = UIColor(named: "primary01")!
+    static let primary02: UIColor = UIColor(named: "primary02")!
+    static let primary03: UIColor = UIColor(named: "primary03")!
+    static let primary04: UIColor = UIColor(named: "primary04")!
+    static let secondary01: UIColor = UIColor(named: "secondary01")!
+    static let secondary02: UIColor = UIColor(named: "secondary02")!
+    static let thirdly01: UIColor = UIColor(named: "thirdly01")!
+    static let thirdly02: UIColor = UIColor(named: "thirdly02")!
+    static let thirdly03: UIColor = UIColor(named: "thirdly03")!
+    
+    static let grey01: UIColor = UIColor(named: "grey01")!
+    static let grey02: UIColor = UIColor(named: "grey02")!
+    static let grey03: UIColor = UIColor(named: "grey03")!
+    
+    static let white: UIColor = UIColor(named: "white")!
+    static let black: UIColor = UIColor(named: "black")!
+}
